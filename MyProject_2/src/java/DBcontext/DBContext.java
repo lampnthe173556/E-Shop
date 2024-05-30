dfb9d2fe3d33c6dfb1a4688d9a1d517938f2f517
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2024-05-30 19:01:03 +0700

    Update DBContext.java

--- MyProject_2/src/java/DBcontext/DBContext.java
+++ MyProject_2/src/java/DBcontext/DBContext.java
@@ -20,7 +20,7 @@ public class DBContext {
             // Edit URL , username, password to authenticate with your MS SQL Server
             String url = "jdbc:sqlserver://localhost:1433;databaseName= MyProject";
             String username = "sa";
-            String password = "lamlam276762";
+            String password = "xxxx";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
            
